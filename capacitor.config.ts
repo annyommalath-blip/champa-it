8238d02c59e425c8bdec908339b4b6efb8ea76cb
annyommalath-blip <annyommalath@gmail.com>
2026-07-23 20:47:08 -0700

    Add Capacitor iOS project

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'co.median.ios.pwoenzz',
+  appName: 'ChampaIT',
+  webDir: 'dist'
+};
+
+export default config;
